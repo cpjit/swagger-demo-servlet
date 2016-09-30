1ae8d60a40de0d3037d71f1998fd28301908cd48
雍欢 <2232911026@qq.com>
2016-09-30 13:38:28 +0800

    支持cpj-swagger v1.2.1

---
+++ src/main/java/com/cpj/swagger/demo/servlet/DemoServlet.java
@@ -0,0 +1,46 @@
+package com.cpj.swagger.demo.servlet;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+import com.alibaba.fastjson.JSONWriter;
+import com.cpj.swagger.annotation.API;
+import com.cpj.swagger.annotation.APIs;
+import com.cpj.swagger.annotation.Param;
+
+
+@APIs("/demo")
+@WebServlet(name = "demoServlet", urlPatterns = { "/demo" })
+@MultipartConfig()
+public class DemoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@API(value="", summary="示例1", parameters={
+			@Param(name="username", description="用户名", type="string"),
+			@Param(name="password", description="密码", type="string", format="password"),
+			@Param(name="image" , description="图片", type="file", format="binary")
+	})
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("application/json;charset=utf-8");
+		JSONWriter writer = new JSONWriter(response.getWriter());
+		Map<String, String> user = new HashMap<String, String>();
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		Part file = request.getPart("file");
+		user.put("username", username);
+		user.put("password", password);
+		writer.writeObject(user);
+		writer.flush();
+		writer.close();
+	}
+
+}
